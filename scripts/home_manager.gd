71839ac051abe887cabd3f859d68066a107a3fd8
Trulle1234 <leonard.trulsson@gmail.com>
2026-09-26 14:12:31 +0200

    add icon, pride month ester egg rework home manager

--- scripts/home_manager.gd
+++ scripts/home_manager.gd
@@ -12,8 +12,7 @@ const TEXT_TEMPLATE = "
 [font_size=18.72][b][url=https://text.npr.org][color=#0000ee]NPR Text[/color][/url]  •  [url=https://news.ycombinator.com/][color=#0000ee]Hacker News[/color][/url]  •  [url=https://brutalist.report/][color=#0000ee]The Brutalist Report[/color][/url]  •  [url=https://en.wikipedia.org/wiki/Main_Page][color=#0000ee]English Wikipedia[/color][/url]  •  [url=https://apod.nasa.gov/apod/astropix.html][color=#0000ee]Astronomy Picture of the Day[/color][/url]  •  [url=https://www.gutenberg.org/][color=#0000ee]Project Gutenberg[/color][/url][/b][/font_size]
 "
 
-# get and format date and clock
-func get_clock():
+func _on_browser_set_home() -> void:
 	var dt = Time.get_datetime_dict_from_system()
 	
 	var month_str = str(dt.month) if dt.month >= 10 else "0" + str(dt.month)
@@ -23,8 +22,11 @@ func get_clock():
 	var date = str(dt.year) + "-" + month_str + "-" + day_str
 	var time = str(dt.hour) + ":" + minute_str
 	
-	return date + " - " + time
-
-func _on_browser_set_home() -> void:
 	document.clear()
-	document.append_text(TEXT_TEMPLATE.replace("[time]", get_clock()))
+	var new_text = TEXT_TEMPLATE.replace("[time]", date + " - " + time)
+	
+	if month_str == "06":
+		new_text = new_text.replace("[wave][font_size=32][b]Godot browser[/b][/font_size][/wave]",
+			"[rainbow freq=0.2 speed=1.0 sat=0.9 val=0.9][wave][font_size=32][b]Godot browser[/b][/font_size][/wave][/rainbow]")
+	
+	document.append_text(new_text)
